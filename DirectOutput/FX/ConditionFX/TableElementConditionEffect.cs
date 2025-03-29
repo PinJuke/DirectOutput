8a5475f45d4ecbb3e914cf5eb5e7a52d71b09c18
Matthias Brendel <mcbrendel@gmx.de>
2025-03-29 19:03:32 +0100

    Update code for Flee 2.0.0 (Dotnet)

--- DirectOutput/FX/ConditionFX/TableElementConditionEffect.cs
+++ DirectOutput/FX/ConditionFX/TableElementConditionEffect.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using DirectOutput.Table;
-using Ciloci.Flee;
 using System.Xml.Serialization;
+using Flee.PublicTypes;
 
 namespace DirectOutput.FX.ConditionFX
 {
